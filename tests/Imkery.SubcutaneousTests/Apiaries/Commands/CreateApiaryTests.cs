ecd0a7e0bce9196d7dfcb871f429c43f6ca0c9c6
Maurice Ponte <maurice.leon.ponte@gmail.com>
2024-05-28 23:03:13 +0200

    added subcutaneous testing project. added framework for first test

---
+++ tests/Imkery.SubcutaneousTests/Apiaries/Commands/CreateApiaryTests.cs
@@ -0,0 +1,14 @@
+class CreateApiaryTests
+{
+    public void CreateApiary_ShouldCreateApiary_WhenCommandIsValid()
+    {
+        // Arrange
+        // Create a valid CreateApiaryCommand
+
+        // Act
+        // Send the CreateApiaryCommand
+
+        // Assert
+        // The result is a apiary correspondig to the details in the create apiary command
+    }
+}
