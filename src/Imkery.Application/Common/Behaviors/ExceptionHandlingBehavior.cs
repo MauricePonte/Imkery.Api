c825fc13a2b54c5bfd23691269f1458d63f3a15a
Maurice Ponte <maurice.leon.ponte@gmail.com>
2024-06-02 23:04:45 +0200

    added exceptionHandling behavior

---
+++ src/Imkery.Application/Common/Behaviors/ExceptionHandlingBehavior.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Imkery.Application.Common.Behaviors;
+
+public class ExceptionHandlingBehavior<TRequest, TResponse>(
+    ILogger<ExceptionHandlingBehavior<TRequest, TResponse>> _logger
+)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : class
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Unhandled exception for {RequestName}", typeof(TRequest).Name);
+            throw;
+        }
+    }
+}
